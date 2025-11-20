31cbf43b9e64963a6c7931452bbdcc43ebb21a34
MostafaAlaa74 <moalaa123321@gmail.com>
2025-11-20 12:06:24 +0200

    Adding a comment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        //? Prevent Lazy Loading (N+1 Problem)
         Model::preventLazyLoading();
     }
 }
-
